91440c378733722de527fa080f2587650516a3a0
znDyz <32562424@qq.com>
2020-03-26 10:06:39 +0800

    修改登陆策略

--- App.vue
+++ App.vue
@@ -7,16 +7,15 @@
 			//app前台运行时调用，app打开验证token，如果验证通过直接跳转index页面，
 			//验证未通过，清除本地token，跳转登录页面
 			var token = uni.getStorageSync("token");
-			console.log("onShow方法中获取的token="+token);
 			if(token!="" && token!=null && token!=undefined){
 				this.iGlobal.request({
 				    url:'/checkToken', method:'POST',
 				}).then((res)=>{
-					console.log("onshou方法返回值："+JSON.stringify(res));
 					if(res.code==200){
-						uni.reLaunch({ url: "./pages/index/index"});
+						//console.log("onshou方法返回值中包含的用户信息："+JSON.stringify(res.data));
+						uni.setStorageSync("user",res.data);
 					}else{
-						uni.getStorageSync("");
+						uni.setStorageSync("token","");
 					}
 				})
 			}
